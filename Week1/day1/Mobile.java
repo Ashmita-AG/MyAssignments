0ce7203417e4ae19463078a7f95ac201579ceb8d
Ashmita-AG <124462803+Ashmita-AG@users.noreply.github.com>
2023-02-04 13:02:15 -0600

    Add files via upload

---
+++ Week1/day1/Mobile.java
@@ -0,0 +1,23 @@
+package Week1.day1;
+
+public class Mobile {
+
+	public static void main(String[] args)
+	{
+		String mobileBrandName="Samsung";
+		char mobileLogo = '@';
+		short noOfMobilePiece=23;
+		int modelNumber=6;
+		long mobileEiNumber=123456789l;
+		float mobilePrice=2500.50f;
+		boolean isDamage=false;
+		System.out.println("Mobile Brand Name :" + mobileBrandName );
+		System.out.println("Mobile Logo : " + mobileLogo);
+		System.out.println("Number of Mobile Piece : " +noOfMobilePiece );
+		System.out.println("Model Number : " + modelNumber );
+		System.out.println("Mobile Ei Number : " + mobileEiNumber );
+		System.out.println("Mobile Price : " + mobilePrice);
+		System.out.println("Is it Damage " + isDamage );
+	}
+
+}
