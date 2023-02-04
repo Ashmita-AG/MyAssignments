62785945af90a45bb97ce64e1249c46102ebf579
Ashmita-AG <124462803+Ashmita-AG@users.noreply.github.com>
2023-02-04 13:00:23 -0600

    Add files via upload

---
+++ day1/HelloSelenium.java
@@ -0,0 +1,19 @@
+package Week1.day1;
+
+public class HelloSelenium {
+	public static void main(String[] args) 
+	{
+		System.out.println("hai");
+		System.out.println("world");
+		int i=5;
+		boolean b=true;
+		String c="welcome";
+		System.out.println(i+ "is integer");
+		System.out.println(b+ " is boolean");
+		System.out.println(c +"is string");
+	
+		
+		
+	}
+
+}
---
+++ day1/TwoWheeler.java
@@ -0,0 +1,23 @@
+package Week1.day1;
+
+public class TwoWheeler 
+{
+
+
+	public static void main(String[] args) 
+	{int noOfWheels=2;
+	short noOfMirrors=123;
+	long chasisNumber=1234568l;
+	boolean isPunctured=true;
+	String bikeName="pulsor";
+	double runningKM=23452345.2349875283475;
+		System.out.println("Number of wheels "+ noOfWheels );
+		System.out.println("Number of mirrors "+ noOfMirrors );
+		System.out.println("Chasis Number is "+chasisNumber );
+		System.out.println("Is it Punctured "+ isPunctured );
+		System.out.println("Bike Number "+ bikeName );
+		System.out.println("Running KMNumber is"+ 23452345.234987528347 );
+		
+	}
+
+}
